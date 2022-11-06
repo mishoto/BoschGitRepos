b3aace89f4b56d629ab9f64624ac53d2f8151060
mishoto <mihail.dev.io@gmail.com>
2022-11-06 10:04:24 +0200

    Initial Commit

---
+++ src/main/java/mihail/BoschIO/Repos/BoschIoReposApplication.java
@@ -0,0 +1,13 @@
+package mihail.BoschIO.Repos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschIoReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschIoReposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mihail/BoschIO/Repos/BoschIoReposApplicationTests.java
@@ -0,0 +1,13 @@
+package mihail.BoschIO.Repos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoschIoReposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
