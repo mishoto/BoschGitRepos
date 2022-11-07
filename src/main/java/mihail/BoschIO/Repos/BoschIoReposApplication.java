440ae7e33758db0bc21f066f9a0113bd03bd155d
mishoto <mihail.dev.io@gmail.com>
2022-11-07 13:39:12 +0200

    add commandLineRunner for initial fetch of data & some logs

--- src/main/java/mihail/BoschIO/Repos/BoschIoReposApplication.java
+++ src/main/java/mihail/BoschIO/Repos/BoschIoReposApplication.java
@@ -1,13 +1,34 @@
 package mihail.BoschIO.Repos;
 
+import mihail.BoschIO.Repos.models.BoschIoGithubRepo;
+import mihail.BoschIO.Repos.service.BoschIoReposClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BoschIoReposApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BoschIoReposApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(BoschIoReposApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(BoschIoReposClientService boschIoReposClientService){
+		return args -> {
+		List<BoschIoGithubRepo> repos = boschIoReposClientService.getBoschIoRepos();
+		log.info("List with {} repos are fetched", repos.size());
+
+		String message = boschIoReposClientService.writeJsonDataToFile(repos);
+		log.info(message);
+		};
+	}
 }
