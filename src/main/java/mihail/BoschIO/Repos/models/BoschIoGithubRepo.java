475ca72c4329d4f03a0e54b760d119072d0fa71b
mishoto <mihail.dev.io@gmail.com>
2022-11-07 19:50:19 +0200

    minor changes

--- src/main/java/mihail/BoschIO/Repos/models/BoschIoGithubRepo.java
+++ src/main/java/mihail/BoschIO/Repos/models/BoschIoGithubRepo.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BoschIoGithubRepo {
 
-    private int id;
-    private String full_name;
-    private String description;
-    private String url;
-    private String language;
+    private final int id;
+    private final String full_name;
+    private final String description;
+    private final String url;
+    private final String language;
 
     public BoschIoGithubRepo(int id, String full_name, String description, String url, String language) {
         this.id = id;
@@ -24,39 +24,19 @@ public class BoschIoGithubRepo {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getFull_name() {
         return full_name;
-    }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getLanguage() {
         return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
     }
 }
