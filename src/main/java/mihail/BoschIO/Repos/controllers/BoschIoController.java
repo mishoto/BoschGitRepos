1143a05aef894230f4174d4c05dc3b930bb3b515
mishoto <mihail.dev.io@gmail.com>
2022-11-07 19:53:33 +0200

    add MediaType change @PathVariable to @RequestParam add GET request with allReposFromFile

--- src/main/java/mihail/BoschIO/Repos/controllers/BoschIoController.java
+++ src/main/java/mihail/BoschIO/Repos/controllers/BoschIoController.java
@@ -3,10 +3,10 @@ package mihail.BoschIO.Repos.controllers;
 import mihail.BoschIO.Repos.models.BoschIoGithubRepo;
 import mihail.BoschIO.Repos.service.BoschIoReposClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -15,27 +15,33 @@ public class BoschIoController {
     @Autowired
     BoschIoReposClientService boschIoReposClientService;
 
-    @GetMapping("/bosch-io-repos/all")
-    public ResponseEntity<List<BoschIoGithubRepo>> getAllRepos(){
+    @GetMapping(path = "/bosch-io-repos/all-from-github-api", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<BoschIoGithubRepo>> getAllReposFromGithub(){
         return ResponseEntity.ok()
                 .body(boschIoReposClientService.getBoschIoRepos());
     }
 
-    @GetMapping("/bosch-io-repos/{fullName}")
-    public ResponseEntity<BoschIoGithubRepo> filterReposByFullName(@PathVariable String fullName ){
+    @GetMapping(path = "/bosch-io-repos/all-from-json-file", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<BoschIoGithubRepo>> getAllReposFromFile(){
+        return ResponseEntity.ok()
+                .body(boschIoReposClientService.getAllBoschIoReposFromFile());
+    }
+
+    @GetMapping(path = "/bosch-io-repos/filter-by-name", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<BoschIoGithubRepo> filterReposByFullName(@RequestParam("fullName") String fullName ){
         return ResponseEntity.ok()
                 .body(boschIoReposClientService.filterBoschIoReposByFullName(fullName));
     }
 
-    @GetMapping("/bosch-io-repos/{description}")
-    public ResponseEntity<BoschIoGithubRepo> filterReposByDescription(@PathVariable String description ){
+    @GetMapping(path = "/bosch-io-repos/filter-by-description", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<BoschIoGithubRepo> filterReposByDescription(@RequestParam("description") String description ){
         return ResponseEntity.ok()
                 .body(boschIoReposClientService.filterBoschIoReposByDescription(description));
     }
 
-    @GetMapping("/bosch-io-repos/{language}")
-    public ResponseEntity<List<BoschIoGithubRepo>> filterReposByLanguage(@PathVariable String language ){
+    @GetMapping(path = "/bosch-io-repos/filter-by-language", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<BoschIoGithubRepo>> filterReposByLanguage(@RequestParam("language") String language ){
         return ResponseEntity.ok()
-                .body(boschIoReposClientService.filterBoschIoReposByLanguage(language));
+                        .body(boschIoReposClientService.filterBoschIoReposByLanguage(language));
     }
 }
